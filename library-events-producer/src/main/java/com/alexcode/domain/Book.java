a62b2db592f1a65b98306d6e7f008dabe245542d
Alex Won <dnjscksdn98@gmail.com>
2020-10-02 15:45:21 +0900

    chore: create library events domain

---
+++ library-events-producer/src/main/java/com/alexcode/domain/Book.java
@@ -0,0 +1,51 @@
+package com.alexcode.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter(value = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Book {
+
+  private Long id;
+
+  private String name;
+
+  private String author;
+
+  public static BookBuilder getBuilder() {
+    return new BookBuilder();
+  }
+
+  public static class BookBuilder {
+    private Long id;
+    private String name;
+    private String author;
+
+    public BookBuilder withId(Long id) {
+      this.id = id;
+      return this;
+    }
+
+    public BookBuilder withName(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public BookBuilder withAuthor(String author) {
+      this.author = author;
+      return this;
+    }
+
+    public Book build() {
+      Book book = new Book();
+      book.setId(id);
+      book.setName(name);
+      book.setAuthor(author);
+      return book;
+    }
+  }
+}
---
+++ library-events-producer/src/main/java/com/alexcode/domain/LibraryEvent.java
@@ -0,0 +1,42 @@
+package com.alexcode.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter(value = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LibraryEvent {
+
+  private Long id;
+
+  private Book book;
+
+  public static LibraryEventBuilder getBuilder() {
+    return new LibraryEventBuilder();
+  }
+
+  private static class LibraryEventBuilder {
+    private Long id;
+    private Book book;
+
+    public LibraryEventBuilder withId(Long id) {
+      this.id = id;
+      return this;
+    }
+
+    public LibraryEventBuilder withBook(Book book) {
+      this.book = book;
+      return this;
+    }
+
+    public LibraryEvent build() {
+      LibraryEvent libraryEvent = new LibraryEvent();
+      libraryEvent.setId(id);
+      libraryEvent.setBook(book);
+      return libraryEvent;
+    }
+  }
+}
