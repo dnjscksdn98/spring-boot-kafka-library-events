01c7db292cbfd286070990fb483c32842eedd68b
Alex Won <dnjscksdn98@gmail.com>
2020-10-02 23:23:52 +0900

    feat: send messages using kafka template

---
+++ library-events-producer/src/main/java/com/alexcode/controller/LibraryEventsController.java
@@ -0,0 +1,37 @@
+package com.alexcode.controller;
+
+import com.alexcode.domain.LibraryEvent;
+import com.alexcode.producer.LibraryEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+@Slf4j
+@RestController
+public class LibraryEventsController {
+
+  @Autowired
+  private LibraryEventProducer libraryEventProducer;
+
+  @PostMapping(path = "/v1/library/event")
+  public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent)
+          throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+    log.info("Before-sendLibraryEvent");
+//    libraryEventProducer.sendLibraryEvent(libraryEvent);
+    SendResult<Long, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+    log.info("SendResult: {}", sendResult.toString());
+    log.info("After-sendLibraryEvent");
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+  }
+}
