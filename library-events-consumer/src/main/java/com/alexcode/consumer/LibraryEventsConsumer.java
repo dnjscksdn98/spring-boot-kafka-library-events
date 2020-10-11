2b3e42bd4c8bd153064563c1ef41a82c24e46a3b
Alex Won <dnjscksdn98@gmail.com>
2020-10-11 20:48:16 +0900

    feat: setup kafka consumer using @KafkaListener

---
+++ library-events-consumer/src/main/java/com/alexcode/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,17 @@
+package com.alexcode.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LibraryEventsConsumer {
+
+	@KafkaListener(topics = {"library-events"})
+	public void onMessage(ConsumerRecord<Long, String> consumerRecord) {
+		log.info("Consumer Record: {}", consumerRecord);
+	}
+}
