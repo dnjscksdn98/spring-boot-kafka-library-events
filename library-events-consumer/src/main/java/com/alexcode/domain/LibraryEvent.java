c7bf851bf169fcc5b14e6e18559d5dd23d06bb5e
Alex Won <dnjscksdn98@gmail.com>
2020-10-13 13:18:20 +0900

    chore: create entities

---
+++ library-events-consumer/src/main/java/com/alexcode/domain/LibraryEvent.java
@@ -0,0 +1,64 @@
+package com.alexcode.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LibraryEvent {
+
+	@Id
+	@GeneratedValue
+  private Long id;
+
+	@ToString.Exclude
+	@OneToOne(fetch = FetchType.LAZY, mappedBy = "libraryEvent", cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+  private Book book;
+
+  @Enumerated(value = EnumType.STRING)
+  private LibraryEventType type;
+
+  public void updateLibraryEventType(LibraryEventType type) {
+    this.setType(type);
+  }
+
+  public static LibraryEventBuilder getBuilder() {
+    return new LibraryEventBuilder();
+  }
+
+  public static class LibraryEventBuilder {
+    private Long id;
+    private Book book;
+
+    public LibraryEventBuilder withId(Long id) {
+      this.id = id;
+      return this;
+    }
+
+    public LibraryEventBuilder withBook(Book book) {
+      this.book = book;
+      return this;
+    }
+
+    public LibraryEvent build() {
+      LibraryEvent libraryEvent = new LibraryEvent();
+      libraryEvent.setId(id);
+      libraryEvent.setBook(book);
+      return libraryEvent;
+    }
+  }
+}
